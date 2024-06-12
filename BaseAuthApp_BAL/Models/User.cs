52e584996041338db3e40b6a3b9ea5d238574825
basileia <jane.novak@centrum.cz>
2024-06-12 21:59:26 +0200

    Add project files.

---
+++ BaseAuthApp_BAL/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAuthApp_BAL.Models
+{
+    public class User
+    {
+    }
+}
