cdebdde26c5199684da6ea56501e1e16bbef5c62
basileia <jane.novak@centrum.cz>
2024-06-14 21:42:25 +0200

    Add Automapper

---
+++ BaseAuthApp_BAL/Extensions/AppMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BaseAuthApp_BAL.Models;
+using BaseAuthApp_DAL.Entities;
+
+
+namespace BaseAuthApp_BAL.Extensions
+{
+    public class AppMapperProfile : Profile
+    {
+        public AppMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<UserCreateModel, User>();
+        }
+    }
+}
