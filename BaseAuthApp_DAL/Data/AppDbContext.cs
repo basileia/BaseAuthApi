5bfba3f543555418c56d22938fd9c302fe8efdd6
basileia <jane.novak@centrum.cz>
2024-06-15 17:01:22 +0200

    Add AppDbContext

---
+++ BaseAuthApp_DAL/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using BaseAuthApp_DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseAuthApp_DAL.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {        
+        public DbSet<User> Users { get; set; }        
+    }
+}
