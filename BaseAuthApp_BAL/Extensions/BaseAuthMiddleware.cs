9d3dd0bd6849ddcbbf2da681e1c35990b5323b26
basileia <jane.novak@centrum.cz>
2024-06-16 22:30:05 +0200

    controls if header contains key Authorization

---
+++ BaseAuthApp_BAL/Extensions/BaseAuthMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAuthApp_BAL.Extensions
+{
+    public class BaseAuthMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (!context.Request.Headers.ContainsKey("Authorization"))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Authorization header missing.");
+                return;
+            }
+        }
+    }
+}
