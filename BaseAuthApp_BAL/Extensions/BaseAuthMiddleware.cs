a6146d35d53df89b8489b62cc21e86bac601bce5
basileia <jane.novak@centrum.cz>
2024-06-22 14:47:19 +0200

    validate user in BaseAuthMiddleware

--- BaseAuthApp_BAL/Extensions/BaseAuthMiddleware.cs
+++ BaseAuthApp_BAL/Extensions/BaseAuthMiddleware.cs
@@ -1,17 +1,22 @@
-﻿using BaseAuthApp_DAL.Contracts;
+﻿using BaseAuthApp_BAL.Services;
+using BaseAuthApp_DAL.Contracts;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Text;
 
 namespace BaseAuthApp_BAL.Extensions
+
+    //pořešit error handler
 {
     public class BaseAuthMiddleware : IMiddleware
     {
         private readonly IRepositoryUser _repositoryUser;
+        private readonly ServiceUser _serviceUser;
 
-        public BaseAuthMiddleware(IRepositoryUser repositoryUser)
+        public BaseAuthMiddleware(IRepositoryUser repositoryUser, ServiceUser serviceUser)
         {
             _repositoryUser = repositoryUser;
+            _serviceUser = serviceUser;
         }
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
@@ -37,16 +42,22 @@ namespace BaseAuthApp_BAL.Extensions
             var username = credentials[0];
             var password = credentials[1];
 
-            //if (!IsAuthorized(username, password))  
-            //{
-            //    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            //    await context.Response.WriteAsync("Invalid username or password.");
-            //    return;
-            //}
+            if (!await _serviceUser.ValidateUserAsync(username, password))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Invalid username or password.");
+                return;
+            }
+
+            if (await _serviceUser.ValidateUserAsync(username, password))
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, username) };
+                var identity = new ClaimsIdentity(claims, "Basic");
+                context.User = new ClaimsPrincipal(identity);
+            }
 
             await next(context);
         }
-
         
     }
 }
