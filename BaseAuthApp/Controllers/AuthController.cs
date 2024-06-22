289497fe908614595b522c3d995042990f3ff7e2
basileia <jane.novak@centrum.cz>
2024-06-22 14:58:51 +0200

    DI ServiceUser

---
+++ BaseAuthApp/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using BaseAuthApp_BAL.Models;
+using BaseAuthApp_BAL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseAuthApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ServiceUser _serviceUser;
+
+        public AuthController(ServiceUser serviceUser)
+        {
+            _serviceUser = serviceUser;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserCreateModel userCreateModel)
+        {
+            
+        }
+
+
+    }
+}
